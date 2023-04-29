78ae91fd8fbb14fd58c5aa0b60fd9c172682635d
HobotM <mateuszhobot@gmail.com>
2023-04-29 21:18:10 +0100

    e

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string response;
+
+System.Console.WriteLine("What is your name?");
+response = Console.ReadLine();
+
+System.Console.WriteLine("Hello " + response);
+
+OperatingSystem thisOs = Environment.OSVersion;
+Console.Write(thisOs.Platform);
+Console.Write(thisOs.VersionString);
