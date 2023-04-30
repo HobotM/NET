81ad9e926178ae40eba8bce435cf9bad8ddddb2d
HobotM <mateuszhobot@gmail.com>
2023-04-30 14:30:52 +0100

    random num gen - output using label

--- RandomNumberGenerator/MyWinFormsApp/Form1.Designer.cs
+++ RandomNumberGenerator/MyWinFormsApp/Form1.Designer.cs
@@ -26,34 +26,48 @@ partial class Form1
     ///  Required method for Designer support - do not modify
     ///  the contents of this method with the code editor.
     /// </summary>
+    /// 
     private void InitializeComponent()
     {
+
+        // create the Label control
+        System.Windows.Forms.Label myLabel = new System.Windows.Forms.Label();
+        myLabel.Name = "myLabel";
+        myLabel.Location = new System.Drawing.Point(10, 10);
+        myLabel.AutoSize = true;
+
         this.generateButton = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // generateButton
-            // 
-            this.generateButton.Location = new System.Drawing.Point(99, 66);
-            this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(100, 23);
-            this.generateButton.TabIndex = 0;
-            this.generateButton.Text = "Generate";
-            this.generateButton.UseVisualStyleBackColor = true;
-            this.generateButton.Click += new System.EventHandler(this.GenerateButton_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 161);
-            this.Controls.Add(this.generateButton);
-            this.Name = "Form1";
-            this.Text = "Random Number Generator";
-            this.ResumeLayout(false);
+        this.SuspendLayout();
+        // 
+        // generateButton
+        // 
+        this.generateButton.Location = new System.Drawing.Point(99, 66);
+        this.generateButton.Name = "generateButton";
+        this.generateButton.Size = new System.Drawing.Size(100, 23);
+        this.generateButton.TabIndex = 0;
+        this.generateButton.Text = "Generate";
+        this.generateButton.UseVisualStyleBackColor = true;
+        this.generateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+        // 
+        // Form1
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(284, 161);
+        this.Controls.Add(this.generateButton);
+        this.Name = "Form1";
+        this.Text = "Random Number Generator";
+        this.ResumeLayout(false);
+        //
+        // label
+        //
+        myLabel.Location = new Point(10, 10); // set the position of the label on the form
+        this.Controls.Add(myLabel);
+
     }
 
     #endregion
 
-    
-        private System.Windows.Forms.Button generateButton;
+
+    private System.Windows.Forms.Button generateButton;
 }
--- RandomNumberGenerator/MyWinFormsApp/Form1.cs
+++ RandomNumberGenerator/MyWinFormsApp/Form1.cs
@@ -7,13 +7,17 @@ public partial class Form1 : Form
     {
 
         InitializeComponent();
-    }
 
-    private void GenerateButton_Click(object sender, EventArgs e)
-    {
-        Random random = new Random();
-        int randomNumber = random.Next(1, 101);
-        MessageBox.Show("Random number: " + randomNumber.ToString(), "Random Number Generator");
+        
+      
     }
+  private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 1001);
+            this.Controls["myLabel"].Text = "Random number: " + randomNumber.ToString();
+        }
+
+
 
 }
